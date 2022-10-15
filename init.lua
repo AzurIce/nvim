c6c749b65c1b736869b7cbda541eccfb071a47cf
AzurIce <973562770@qq.com>
2022-10-15 19:20:42 +0800

    1

--- init.lua
+++ init.lua
@@ -48,6 +48,7 @@ end
 -- Install packer.nvim --
 -------------------------
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+print(install_path)
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
