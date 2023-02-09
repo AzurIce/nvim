3ea2ae95e2cb53448837ea62a44358fda655918c
AzurIce <973562770@qq.com>
2023-02-09 22:51:41 +0800

    changed the colorscheme to material.nvim

--- init.lua
+++ init.lua
@@ -159,7 +159,8 @@ require('packer').startup(function(use)
     -- Colorscheme
     -- use 'mhartington/oceanic-next'
     -- use 'drewtempelmeyer/palenight.vim'
-    use 'sainnhe/edge'
+    -- use 'sainnhe/edge'
+    use 'marko-cerovac/material.nvim'
 
     use {
         'lewis6991/gitsigns.nvim',
@@ -226,19 +227,86 @@ opt.termguicolors = true
 -- Use light theme during 7 ~ 17
 local current_hour = tonumber(os.date("%H"))
 if current_hour >= 7 and current_hour <= 17 then
-    opt.background = 'light'
+    vim.g.material_style = 'lighter'
+    -- opt.background = 'light'
 else
-    opt.background = 'dark'
+    vim.g.material_style = 'palenight'
+    -- opt.background = 'dark'
 end
 
-vim.g.edge_style = 'neon'
-vim.g.edge_better_performance = 1
--- vim.g.edge_transparent_background = 1
-vim.g.edge_dim_inactive_windows = 1
-vim.g.edge_diagnostic_text_highlight = 1
-vim.g.edge_diagnostic_line_highlight = 1
-vim.g.edge_diagnostic_virtual_text = 'colored'
-vim.api.nvim_command 'colorscheme edge'
+-- edge colorscheme
+-- vim.g.edge_style = 'neon'
+-- vim.g.edge_better_performance = 1
+-- -- vim.g.edge_transparent_background = 1
+-- vim.g.edge_dim_inactive_windows = 1
+-- vim.g.edge_diagnostic_text_highlight = 1
+-- vim.g.edge_diagnostic_line_highlight = 1
+-- vim.g.edge_diagnostic_virtual_text = 'colored'
+-- vim.api.nvim_command 'colorscheme edge'
+
+
+require('material').setup({
+    contrast = {
+        terminal = false, -- Enable contrast for the built-in terminal
+        sidebars = false, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+        floating_windows = false, -- Enable contrast for floating windows
+        cursor_line = false, -- Enable darker background for the cursor line
+        non_current_windows = false, -- Enable darker background for non-current windows
+        filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+    },
+
+    styles = { -- Give comments style such as bold, italic, underline etc.
+        comments = { italic = true },
+        strings = { --[[ bold = true ]] },
+        keywords = { --[[ underline = true ]] },
+        functions = { --[[ bold = true, undercurl = true ]] },
+        variables = {},
+        operators = {},
+        types = {},
+    },
+
+    plugins = { -- Uncomment the plugins that you use to highlight them
+        -- Available plugins:
+        -- "dap",
+        -- "dashboard",
+        "gitsigns",
+        -- "hop",
+        -- "indent-blankline",
+        -- "lspsaga",
+        -- "mini",
+        -- "neogit",
+        "nvim-cmp",
+        -- "nvim-navic",
+        "nvim-tree",
+        "nvim-web-devicons",
+        -- "sneak",
+        -- "telescope",
+        -- "trouble",
+        -- "which-key",
+    },
+
+    disable = {
+        colored_cursor = true, -- Disable the colored cursor
+        borders = false, -- Disable borders between verticaly split windows
+        background = false, -- Prevent the theme from setting the background (NeoVim then uses your terminal background)
+        term_colors = false, -- Prevent the theme from setting terminal colors
+        eob_lines = false -- Hide the end-of-buffer lines
+    },
+
+    high_visibility = {
+        lighter = false, -- Enable higher contrast text for lighter style
+        darker = false -- Enable higher contrast text for darker style
+    },
+
+    lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+
+    async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+
+    custom_colors = nil, -- If you want to everride the default colors, set this to a function
+
+    custom_highlights = {}, -- Overwrite highlights with your own
+})
+vim.cmd 'colorscheme material'
 
 vim.g.winblend=10
 if not fn.has('win32') then
