8efabb5d272d707b3a8e045f5899c716cf73cbbc
Azur冰弦 <973562770@qq.com>
2023-02-26 12:08:17 +0800

    restructured the code

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -3,4 +3,17 @@ return {
     require'plugins.ui.bufferline',
     require'plugins.ui.lualine',
     require'plugins.ui.nvim-tree',
+
+    {
+        'norcalli/nvim-colorizer.lua',
+        config = true,
+    },
+
+    {
+        'lewis6991/gitsigns.nvim',
+        config = true,
+        dependencies = {
+            'nvim-lua/plenary.nvim'
+        }
+    },
 }
